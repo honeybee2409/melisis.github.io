dc00911440867a3b6906c9229e903b68e2b42a65
Mel Isis <honeybee7415@gmail.com>
2025-05-15 22:00:15 -0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,5 +1,3 @@
-# A dose ideal de água é calculada através da fórmula
-# a: massa * 0,03
 peso = float(input("Qual o seu peso, em kg? "))
 nec = peso * 0.03
 print("A quantidade necessária de água necessária para um corpo de {}kg é {} litros.".format(peso, nec))
