4b33ef603e0f6bb4a823179ed4fdbc0f5788e29f
Mel Isis <honeybee7415@gmail.com>
2025-05-15 22:00:25 -0300

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,7 +1,3 @@
-# Construa o programa que tendo como dados de entrada dois pontos quaisquer em um plano cartesiano
-# , P(x1, y1) e Q(x2, y2), calcule a distância entre eles. Use a seguinte fórmula:
-# distância = RAIZ(x2 - x1)^2 + (y2 - y1)^2
-
 x1 = int(input('Qual o primeiro ponto x? '))
 y1 = int(input('Qual o primeiro ponto y? '))
 x2 = int(input('Qual o segundo ponto x? '))
