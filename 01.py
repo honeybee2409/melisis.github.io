ec9c6f5160b6a35158d2215050051650c9aa07b0
Mel Isis <honeybee7415@gmail.com>
2025-05-15 21:43:30 -0300

    Delete 01.py

--- 01.py
+++
@@ -1,3 +0,0 @@
-raio = float(input('Digite o valor do raio: '))
-volume = (4/3) * 3.14 * raio ** 3
-print('Arredondando o pi para 3,14, o volume de uma esfera de raio {} é igual à {:.2f}cm³.'.format(raio, volume))
